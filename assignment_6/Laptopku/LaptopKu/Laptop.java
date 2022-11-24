11998de7add0779eae3893e0364429980fea6780
eyO18 <106310176+Ahmad-busaeri@users.noreply.github.com>
2022-11-24 14:24:55 +0700

    Add files via upload

---
+++ assignment_6/Laptopku/LaptopKu/Laptop.java
@@ -0,0 +1,11 @@
+package assignment_6.Laptopku.LaptopKu;
+
+public interface Laptop {
+    int MAX_VOL=100;
+    int MIN_VOL=0;
+
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
